4b8f70d9f5ada0add34bf5d176e39aaa822a473f
Rafael Arquero <rafael@arquero.cat>
2018-10-13 23:55:50 +0200

    dock

---
+++ dock.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+dockutil --no-restart --remove all
+dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/Visual Studio Code.app"
+dockutil --no-restart --add "/Applications/GitUp.app"
+dockutil --no-restart --add "/Applications/Notes.app"
+dockutil --no-restart --add "/Applications/Reminders.app"
+dockutil --no-restart --add "/Applications/Telegram.app"
+dockutil --no-restart --add "/Applications/Hyper.app"
+
+killall Dock
