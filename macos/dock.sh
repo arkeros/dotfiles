07e665533ea8201f65cb54b51f6b271be73959c4
Rafael Arquero <rafael@arquero.cat>
2018-10-30 12:30:24 +0100

    ADD slack to dock

--- macos/dock.sh
+++ macos/dock.sh
@@ -6,6 +6,7 @@ dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/Applications/GitUp.app"
 dockutil --no-restart --add "/Applications/Notes.app"
 dockutil --no-restart --add "/Applications/Reminders.app"
+dockutil --no-restart --add "/Applications/Slack.app"
 dockutil --no-restart --add "/Applications/Telegram.app"
 dockutil --no-restart --add "/Applications/Hyper.app"
 
